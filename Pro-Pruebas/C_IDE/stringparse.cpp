b55c3df29867d081e561a23dc4b21bae3834c7b5
Paola Quirós <66766320+paoqch@users.noreply.github.com>
2021-04-28 12:29:16 -0600

    Update stringparse.cpp

--- Pro-Pruebas/C_IDE/stringparse.cpp
+++ Pro-Pruebas/C_IDE/stringparse.cpp
@@ -1,11 +1,98 @@
 #include "stringparse.h"
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <sys/stat.h>
+#include <iostream>
+#include "QString"
+#include "QFile"
+#include "QTextStream"
+#include <stdlib.h>
+#include <typeinfo>
+#include <ctype.h>
+#include "QJsonObject"
+#include "QJsonDocument"
+#include <unistd.h>
+#include <vector>
+#include "mainwindow.h"
 
 using namespace std;
+using std::string;
 
 StringParse::StringParse()
 {
 
 }
+
+QJsonObject StringParse::parse(vector<string> line){
+    if(i >= line.size()){
+        QJsonObject termino = makeJson("Finish", "Finish", "Finish");
+        return termino;
+    }
+
+ }
+
+QJsonObject StringParse::writeFile(vector <string> line){
+
+    QJsonObject objeto = getType(line);
+    return objeto;
+}
+
+void StringParse::readFile(){
+
+}
+
+QJsonObject StringParse::makeJson(string tip, string val, string var){
+    QJsonObject object
+        {
+            {"Type","NI"},
+            {"Value","NI"},
+            {"Variable","NI"}
+        };
+
+        QVariant tipo(QString::fromStdString(tip));
+        QVariant valor(QString::fromStdString(val));
+        QVariant variable(QString::fromStdString(var));
+
+        object.insert("Type",tipo.toJsonValue());
+        object.insert("Value",valor.toJsonValue());
+        object.insert("Variable",variable.toJsonValue());
+        return object;
+}
+
+QJsonObject StringParse::getType(vector<string> line){
+    do{
+        if(line[0] == "int"){
+                string variable  = getVariable(line);
+                string valor = getValor(line);
+                QJsonObject  objeto = makeJson("int",valor,variable);
+                return objeto;
+                continue;
+        }
+        else if(line[0]=="float"){
+                string variable  = getVariable(line);
+                string valor = getValor(line);
+                QJsonObject  objeto = makeJson("float",valor,variable);
+                return objeto;
+                continue;
+        }
+        else if(line[0]=="double"){
+                string variable  = getVariable(line);
+                string valor = getValor(line);
+                QJsonObject  objeto = makeJson("double",valor,variable);
+                return objeto;
+                continue;
+            }
+        }
+        while(i <= line.size());
+    QJsonObject  objetof = makeJson("Finish","Finish","Finish");
+    return objetof;
+
+}
+
 
 void StringParse::SplitString(const string& texto, char del, vector<string>& v){
 
@@ -523,4 +610,79 @@ string StringParse::GenerarReference(vector<string> line) {
 
 
     return "error";
+}
+
+
+string StringParse::getVariable(vector<string> line) {
+
+    string nombreVariable;
+    string tipoVariable;
+
+    if (line[line.size()-1].find(';') != -1){
+        VerificarPuntoyComa(line);
+    } else if (line.size() >= 4) {
+        return "error";
+    }else {
+        return "error";
+    }
+
+    if(VerificarEspacios(line)){
+
+        int valor = 0;
+        string valor1;
+
+        valor1 = ObtenerNumero(line.at(3));
+
+        if (valor1 != "error"){
+            valor += stoi(valor1);
+        } else{
+            return "error";
+        }
+
+        tipoVariable = "Type " + line[0] + "\n";
+        cout << "Type " + line[0] + "\n";
+        nombreVariable = line[1];
+        cout << "Nombre " + line[1] + "\n";
+        return nombreVariable;
+    }
+}
+
+string StringParse::getValor(vector<string> line) {
+
+    string valorVariable;
+
+    if (line[line.size()-1].find(';') != -1){
+        VerificarPuntoyComa(line);
+    } else if (line.size() >= 4) {
+        return "error";
+    }else {
+        return "error";
+    }
+
+    if(VerificarEspacios(line)){
+
+        int valor = 0;
+        string valor1;
+
+        valor1 = ObtenerNumero(line.at(3));
+
+        if (valor1 != "error"){
+            valor += stoi(valor1);
+        } else{
+            return "error";
+        }
+
+        valorVariable = to_string(valor);
+        cout << "Valor " + to_string(valor)+ "\n";
+        return valorVariable;
+    }
+}
+
+string StringParse::getValorChar(vector<string> line) {
+
+    string valorVariable;
+
+    valorVariable = line[3];
+    cout << "Nombre " + line[3] + "\n";
+    return valorVariable;
 }
