3047f2972f78311f2cc27fd6e56ac95f2df3ff54
Paola Quirós <66766320+paoqch@users.noreply.github.com>
2021-04-28 12:22:29 -0600

    Update stringparse.h

--- Pro-Pruebas/C_IDE/stringparse.h
+++ Pro-Pruebas/C_IDE/stringparse.h
@@ -3,18 +3,38 @@
 
 #include <QtCore/qglobal.h>
 
+#include "client.h"
 #include <string>
 #include <vector>
 #include <iostream>
 #include <functional>
 #include <sstream>
+#include "QJsonObject"
+#include "QJsonDocument"
+#include <string.h>
 
 using namespace std;
+using std::string;
 
 class StringParse
 {
 public:
+
+
+    StringParse *convertidor = new StringParse();
+
+    vector<string> linesCode;
+    int i = 0;
+    string entero = "int";
+    QJsonObject makeJson(string tip, string val, string var);
+    string codigoCompleto;
+    QJsonObject parse(vector <string> line);
+    QJsonObject writeFile(vector <string> line);
+    QJsonObject getType(vector <string> line);
+    void readFile();
+
     StringParse();
+
     void SplitString(const string& texto, char del, vector<string>& v);
     string EliminarEspacios(string texto);
     bool VerificarEspacios(vector <string> v);
@@ -29,6 +49,11 @@ public:
     string GenerarCout(vector <string> line);
     string ObtenerNumero(string name);
     void VerificarPuntoyComa(vector<string>& line);
+
+
+    string getValor(vector<string> line);
+    string getValorChar(vector<string> line);
+    string getVariable(vector<string> line);
 };
 
 #endif // STRINGPARSE_H
