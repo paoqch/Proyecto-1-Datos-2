234c0feca334f495cac2427f6cb56a47485a7aad
Paola Quirós <66766320+paoqch@users.noreply.github.com>
2021-04-29 00:08:35 -0600

    Update client.h

--- Pro-Pruebas/C_IDE/client.h
+++ Pro-Pruebas/C_IDE/client.h
@@ -1,13 +1,17 @@
 #ifndef CLIENT_H
 #define CLIENT_H
-
 #include <QtNetwork>
 #include <QObject>
 #include <QtNetwork/QTcpSocket>
 
-//se comunica con el Server y el IDE
+
+/**
+ * @brief The Client class
+ * Clase que se comunica con el Server, mantiene relación directa con el IDE y el parser
+ */
 class Client : public QObject{
     Q_OBJECT
+
 
 public:
     Client(QObject * obj = 0,QString add="localhost", quint16 port = 4000);
@@ -24,4 +28,4 @@ private:
     QTcpSocket *socket;
 };
 
-#endif // CLIENT_H
+#endif
