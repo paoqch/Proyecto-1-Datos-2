67ea8fe39ee9e4e024f5131ca5008a1cc3ca9c98
Paola Quirós <66766320+paoqch@users.noreply.github.com>
2021-04-28 23:59:09 -0600

    Update variables.cpp

--- Pro-Pruebas/Mserver/variables.cpp
+++ Pro-Pruebas/Mserver/variables.cpp
@@ -7,13 +7,20 @@
 #include <string>
 #include <cmath>
 
-//
 
 using namespace std;
-
+/**
+ * @brief variables::variables
+ * Clase encargada de procesar las solicitudes del cliente
+ */
 variables::variables(){
 }
-
+/**
+ * @brief variables::preparation
+ * @param object
+ * @return
+ * Analiza los parámetros del objeto JSON object
+ */
 QString variables::preparation(QJsonObject object){
         QString type = QString(object.value("Type").toString());
         QString label = QString(object.value("Variable").toString());
@@ -24,6 +31,8 @@ QString variables::preparation(QJsonObject object){
             value = atoi(analisis.c_str());
             qDebug()<<"Nodo insertado...";
             valor = QString::fromStdString(to_string(value).c_str());
+            cout<<"Valor preparation " + value;
+
 
         }else if (type == QString("double")){
             string analisis = analizarValor(object.value("Value").toString().toUtf8().constData(), type.toUtf8().constData());
@@ -39,21 +48,28 @@ QString variables::preparation(QJsonObject object){
         return valor;
 }
 
-
+/**
+ * @brief variables::analizarValor
+ * @param valor
+ * @param tipo
+ * @return
+ * Analiza si valor cumple con la definición de tipo
+ */
 string variables::analizarValor(string valor, string tipo){
 
     if (tipo == "int"){
         return valor;
+        cout<<"Valor analizar "+valor;
     }
 
     else if (tipo == "double"){
         return valor;
     }
-    
+
     else if (tipo == "float"){
         return valor;
     }
-    
+
     else if (tipo == "long"){
         return valor;
     }
